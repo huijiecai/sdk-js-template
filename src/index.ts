5b8cda1a1015702482eb7fcbf5696629c7ba4c8f
huijiecai <93887983+huijiecai@users.noreply.github.com>
2022-11-07 11:32:30 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,9 @@ console.log("dfa");
 new Person("huijie").fly();
 console.log(new Person("huijie1").getName());
 console.log("ddd");
+
+function sum(a: number, b: number): number {
+    return a + b;
+}
+
+export default sum;
